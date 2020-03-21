d4f54645f7c05cfa5474b74a58281311b9d23d34
JayV20 <racelo_09@hotmail.com>
2020-03-21 13:08:21 -0600

    Creación del modelo User y justes en el login (color del boton y propiedad end icon mode para borrar el contenido del EditText)

---
+++ app/src/main/java/com/example/prefecturailb/models/User.java
@@ -0,0 +1,56 @@
+package com.example.prefecturailb.models;
+
+import java.util.Objects;
+
+public class User {
+    private  String name;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return name.equals(user.name) &&
+                email.equals(user.email) &&
+                password.equals(user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, email, password);
+    }
+}
